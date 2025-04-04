6f2a00fabe660160606e328574d1e199df01ad04
Samuel Damasceno <smkdo04@gmail.com>
2025-04-04 13:41:13 -0300

    Criando projeto

---
+++ src/main/java/br/com/samuel/tabelafibe/TabelafibeApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.tabelafibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelafibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelafibeApplication.class, args);
+	}
+
+}
